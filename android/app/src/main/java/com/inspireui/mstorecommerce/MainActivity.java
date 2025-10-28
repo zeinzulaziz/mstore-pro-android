27b026c41fe5487c74879370034c2f71e3fd2703
zein <zein@fanaloka.co>
2025-10-28 07:45:34 +0700

    fix: update app name configuration for Android
    - Update app.json with consistent app name 'MStoreProAndroid'
    - Update MainActivity.java to use correct component name
    - Update strings.xml with proper app display name
    - Fix 'MStore_v2' registration error

--- android/app/src/main/java/com/inspireui/mstorecommerce/MainActivity.java
+++ android/app/src/main/java/com/inspireui/mstorecommerce/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "MStore_v2";
+    return "MStoreProAndroid";
   }
 
   /**
